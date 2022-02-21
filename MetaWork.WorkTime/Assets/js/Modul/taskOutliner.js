7e96af3e30702d7dfa1e613a5a7508933cff2df3
cuongvhelands <98455223+cuongvhelands@users.noreply.github.com>
2022-02-21 14:17:14 +0700

    Khoi tao

---
+++ MetaWork.WorkTime/Assets/js/Modul/taskOutliner.js
@@ -0,0 +1,20 @@
+﻿var idtr;
+var projectIdM = $("#projectIdM").val();
+$(document).on("click", "tr ", function () {
+    idtr = this.id;
+    $(this).addClass("selected").siblings().removeClass("selected");
+})
+function CreateNewTask() {
+    GetTrId();
+}
+$(document).on("click", ".addNewTaskOutliner", function () {
+    CreateNewTask();
+})
+function GetTrId() {
+    var tr = $('#tableTaskOutline tr.selected');
+    if (!tr || tr.length == 0 || tr[0].id == "tr-" + projectIdM + "-0") {
+
+    } else {
+
+    }
+}
